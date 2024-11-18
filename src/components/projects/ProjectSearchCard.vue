aa65de3afc223d95fb232901f3914aaa55d22658
Anthony Hatch <ahatch1490@gmail.com>
2024-11-17 19:13:47 -0800

    fixes from ian

--- src/components/projects/ProjectSearchCard.vue
+++ src/components/projects/ProjectSearchCard.vue
@@ -5,10 +5,10 @@ import Card from 'primevue/card';
 <template>
   <Card style="width: 25rem; overflow: hidden" class="rounded-b-xl ">
     <template #header>
-      <img class="rounded-t-lg " alt="user header" src="../../../public/widget.webp" />
+      <img alt="user header" src="../../../public/widget.webp" />
     </template>
-    <template #title><h1 class="font-bold text-2xl">Advanced Card</h1></template>
-    <template #subtitle><h2 class="text-xl">Card subtitle</h2></template>
+    <template #title>Advanced Card</template>
+    <template #subtitle>Card subtitle</template>
     <template #content>
       <p class="m-0">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
@@ -16,7 +16,10 @@ import Card from 'primevue/card';
       </p>
     </template>
     <template #footer>
-
+      <div class="flex gap-4 mt-1">
+        <Button label="Cancel" severity="secondary" outlined class="w-full" />
+        <Button label="Save" class="w-full" />
+      </div>
     </template>
   </Card>
 </template>
