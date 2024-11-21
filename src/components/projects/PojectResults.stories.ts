87d2b7e5a80811422fffc888bdb59f3d6baeb689
Anthony Hatch <ahatch1490@gmail.com>
2024-11-21 08:23:15 -0800

    adding story differences

--- src/components/projects/PojectResults.stories.ts
+++ src/components/projects/PojectResults.stories.ts
@@ -24,9 +24,9 @@ Default.args = {projects:[{
     tags: ["tag1","tag2"]
 }, {
         id: "b1aca791-c041-4ed3-b041-265783908f05",
-        title: "FoobarBaz", // The title of the project
+        title: "FoobarBazBuzz", // The title of the project
         description: "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque" +
             "quas!", // A brief description of the project
         imageUrls: ["../../../public/widget.webp"], // An array of image URLs associated with the project
-        tags: ["tag1","tag2"]
+        tags: ["tag1","tag3"]
     }]};
