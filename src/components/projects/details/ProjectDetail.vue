ef6415976591596ccb697728e20e96bef1956129
Anthony Hatch <ahatch1490@gmail.com>
2024-12-02 08:06:43 -0800

    doing some spacing clean up

--- src/components/projects/details/ProjectDetail.vue
+++ src/components/projects/details/ProjectDetail.vue
@@ -9,26 +9,26 @@ const props = defineProps<{projectDetails:ProjectDetails}>();
 
 <template>
 <h1 class="text-4xl font-extrabold">{{props.projectDetails.title}}</h1>
-  <div class="border-2 border-b-gray-600">
-    <dl>
-      <dt class="text-2xl border-b-1" >Description</dt>
-      <dd>{{props.projectDetails.description}}</dd>
-      <dt class="text-2xl mb-2">Materials</dt>
-      <dd class="border-b-2 border-b-gray-600">
+  <div class="flex  border-2 border-gray-200 pl-2">
+    <dl class="max-w-52 grid grid-cols-2 gap-x-4">
+      <dt class="text-sm font-bold mt-4">Materials</dt>
+      <dd class="border-b-gray-600 text-sm font-bold mb-4 mt-4">
         <span v-for="material  in props.projectDetails.materials"
               class="rounded
-              background-blue-50 \
+              background-blue-50
               p-2 m-1 bg-gray-500 border-dashed
               border-b-gray-600"
         >
           {{material}}</span>
       </dd>
-      <dt class="text-2xl mt-2">Start Date</dt>
-      <dd>{{props.projectDetails.startDate}}</dd>
-      <dt class="text-2xl">End Date</dt>
-      <dd>{{props.projectDetails.endDate}}</dd>
-      <dt></dt>
-      <dd>{{props.projectDetails.difficulty}}</dd>
+      <dt class="text-sm font-bold">Start Date</dt>
+      <dd class="text-sm">{{props.projectDetails.startDate}}</dd>
+      <dt class="text-sm font-bold">End Date</dt>
+      <dd class="text-sm">{{props.projectDetails.endDate}}</dd>
+      <dt class="text-sm font-bold">Difficulty</dt>
+      <dd class="text-sm">{{props.projectDetails.difficulty}}</dd>
+      <dt class="text-sm font-bold " >Description</dt>
+      <dd class="text-sm"><p>{{props.projectDetails.description}}</p></dd>
     </dl>
   </div>
 </template>
